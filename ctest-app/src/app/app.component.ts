c074f109a7d1758fd12edc4279e14b81f4b128f7
Clement <clement.trumpff@centraltest.com>
2017-07-26 17:09:42 +0200

    toh day 6

--- ctest-app/src/app/app.component.ts
+++ ctest-app/src/app/app.component.ts
@@ -5,26 +5,11 @@ import { HeroService } from './hero.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent implements OnInit{
   title = 'Tour of heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
-
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-  }
-  
-  getHeroes(): void {
-     this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
-  }
-  
   ngOnInit(): void {
-    this.getHeroes();
   }
-  
-  constructor(private heroService: HeroService) { }
 };
--- ctest-app/src/app/hero.service.ts
+++ ctest-app/src/app/hero.service.ts
@@ -5,13 +5,18 @@ import { HEROES } from './mock-heroes';
 @Injectable()
 export class HeroService {
 
+  getHero(id:number): Promise<Hero> {
+    return this.getHeroes()
+      .then( heroes => heroes.find(hero => hero.id === id));
+  }
+
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
   }
-  
+
   getHeroesSlowly(): Promise<Hero[]> {
     return new Promise (
-        resolve => { 
+        resolve => {
             setTimeout(() => resolve(this.getHeroes()), 2000)
         }
     );
